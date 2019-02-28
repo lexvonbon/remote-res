905d4bd553f0c14f5c2c33e5d40a0884f738a8b4
MCSstu <MCSstu@CGR1174860.cord.local>
2019-02-28 02:28:18 -0600

    Fuck

--- download.ps1
+++ download.ps1
@@ -1,20 +1,11 @@
-PARAM ($inputFile, $downloadFolder)
-
-$textfile = cat $inputFile
+$textfile = cat ".\dlList.txt"
 
 foreach ($item in $textfile) {     
-    $redirected = [System.Net.WebRequest]::Create($item).GetResponse()
-    $redirected.ResponseUri.OriginalString
-    $url = $response.ResponseUri.ToString()
-    $redirected.Close()
-    
-    $split = $url.LastIndexOf('/') + 1
-    $fileName = $url.Substring($split, $url.Length - $split)
-    
-    $destination = "$downloadFolder" + $fileName
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+    $client = New-Object System.Net.WebClient;
 
-    Write-Host $destination " curently downloading"
-
-    (New-Object System.Net.WebClient).DownloadFile($url, $destination)
+    $split = $item.ToString().LastIndexOf("/") + 1;
+    $fileName = $item.ToString().Substring($split, $item.ToString().Length - $split);
+    $client.DownloadFile($item, "C:\Users\MCSstu\Downloads\$filename");
     
 }
